373218ddf3c87c8b296ec3d7ddcc11d119fa0db6
Vivek Wisdom <vivek.wisdom@accenture.com>
2017-01-31 12:58:19 +0530

    CR 321

---
+++ src/vw/codeforces/contests/cr321/KefaAndCompany.java
@@ -0,0 +1,90 @@
+/**
+ * 
+ */
+package vw.codeforces.contests.cr321;
+
+/**
+ * @author vivek
+ *
+ */
+
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.HashMap;
+import java.util.StringTokenizer;
+
+public class KefaAndCompany implements Closeable {
+	private InputReader in = new InputReader(System.in);
+	private PrintWriter out = new PrintWriter(new BufferedOutputStream(System.out));
+
+	public void solve() {
+		int n = in.nextInt();
+		int d = in.nextInt();
+		HashMap<Integer, Integer> ms = new HashMap<>();
+		int maxFactor = 0;
+		for (int i = 0; i < n; i++) {
+			int m = in.nextInt();
+			int s = in.nextInt();
+			ms.put(m, s);
+		}
+		
+	}
+
+	@Override
+	public void close() throws IOException {
+		in.close();
+		out.close();
+	}
+
+	static class InputReader {
+		public BufferedReader reader;
+		public StringTokenizer tokenizer;
+
+		public InputReader(InputStream stream) {
+			reader = new BufferedReader(new InputStreamReader(stream), 32768);
+			tokenizer = null;
+		}
+
+		public int[] nextIntArray(int n) {
+			int[] arr = new int[n];
+			for (int i = 0; i < n; i++) {
+				arr[i] = Integer.parseInt(next());
+			}
+			return arr;
+		}
+
+		public String next() {
+			while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+				try {
+					tokenizer = new StringTokenizer(reader.readLine());
+				} catch (IOException e) {
+					throw new RuntimeException(e);
+				}
+			}
+			return tokenizer.nextToken();
+		}
+
+		public int nextInt() {
+			return Integer.parseInt(next());
+		}
+
+		public long nextLong() {
+			return Long.parseLong(next());
+		}
+
+		public void close() throws IOException {
+			reader.close();
+		}
+	}
+
+	public static void main(String[] args) throws IOException {
+		try (KefaAndCompany instance = new KefaAndCompany()) {
+			instance.solve();
+		}
+	}
+}
---
+++ src/vw/codeforces/contests/cr321/KefaAndFirstSteps.java
@@ -0,0 +1,94 @@
+/**
+ * 
+ */
+package vw.codeforces.contests.cr321;
+
+/**
+ * @author vivek
+ *
+ */
+
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class KefaAndFirstSteps implements Closeable {
+	private InputReader in = new InputReader(System.in);
+	private PrintWriter out = new PrintWriter(new BufferedOutputStream(System.out));
+
+	public void solve() {
+		int n = in.nextInt();
+		int[] arr = in.nextIntArray(n);
+		int count = 0;
+		int total = 0;
+		for (int i = 1; i < arr.length; i++) {
+			if (arr[i] >= arr[i - 1]) {
+				count++;
+				if (count > total) {
+					total = count;
+				}
+			} else {
+				count = 0;
+			}
+		}
+		out.println(total + 1);
+	}
+
+	@Override
+	public void close() throws IOException {
+		in.close();
+		out.close();
+	}
+
+	static class InputReader {
+		public BufferedReader reader;
+		public StringTokenizer tokenizer;
+
+		public InputReader(InputStream stream) {
+			reader = new BufferedReader(new InputStreamReader(stream), 32768);
+			tokenizer = null;
+		}
+
+		public int[] nextIntArray(int n) {
+			int[] arr = new int[n];
+			for (int i = 0; i < n; i++) {
+				arr[i] = Integer.parseInt(next());
+			}
+			return arr;
+		}
+
+		public String next() {
+			while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+				try {
+					tokenizer = new StringTokenizer(reader.readLine());
+				} catch (IOException e) {
+					throw new RuntimeException(e);
+				}
+			}
+			return tokenizer.nextToken();
+		}
+
+		public int nextInt() {
+			return Integer.parseInt(next());
+		}
+
+		public long nextLong() {
+			return Long.parseLong(next());
+		}
+
+		public void close() throws IOException {
+			reader.close();
+		}
+	}
+
+	public static void main(String[] args) throws IOException {
+		try (KefaAndFirstSteps instance = new KefaAndFirstSteps()) {
+			instance.solve();
+		}
+	}
+}
